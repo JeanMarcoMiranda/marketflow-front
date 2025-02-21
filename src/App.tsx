a6a552ce49c52ab343adb27ec5843474a9fe751a
JeanMarcoMiranda <elecord2014@gmail.com>
2025-02-21 11:09:25 -0500

    feat: base project configuration complete

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <div>Buenas</div>
+      <Button>
+        hOLA A TODOS
+      </Button>
+    </>
+  )
+}
+
+export default App
