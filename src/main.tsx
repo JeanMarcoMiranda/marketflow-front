d5fe30be7e105c1e8c3e68f62ad4af5a1dd67016
JeanMarcoMiranda <elecord2014@gmail.com>
2025-03-03 20:44:53 -0500

    feat: Tanstack Query setup

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,18 @@ import { createRoot } from "react-dom/client";
 import { Toaster } from "@/components/ui/sonner";
 import { RouterProvider } from "react-router-dom";
 import { GlobalDialog } from "./shared/components/GlobalDialog";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={Router} />
-    <Toaster />
-    <GlobalDialog />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={Router} />
+      <Toaster />
+      <GlobalDialog />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
